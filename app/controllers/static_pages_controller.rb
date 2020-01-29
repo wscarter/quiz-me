8edc360decd0fe8442538336067a57285101ce81
wscarter <wescarterdev@fastmail.com>
2020-01-28 20:10:49 -0600

    static pages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def welcome
+    respond_to do |format|
+      format.html { render :welcome }
+    end
+  end
+
+  def about
+    respond_to do |format|
+      format.html { render :about }
+    end
+  end
+end
