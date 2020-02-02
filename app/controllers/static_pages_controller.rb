bee5dd0dceec77622baa685b90e46172a776ab2e
wscarter <wescarterdev@fastmail.com>
2020-02-02 12:31:08 -0600

    forms

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,49 @@
 class StaticPagesController < ApplicationController
   def welcome
+    features = [
+      "Choose from premade quizzes on a variety of topics",
+      "Make your own quizzes to customize your learning",
+      "Compare your scores with other users"
+    ]
+
     respond_to do |format|
-      format.html { render :welcome }
+      format.html { render :welcome, locals: { features: features } }
     end
   end
 
   def about
     respond_to do |format|
       format.html { render :about }
+    end
+  end
+
+  def contact
+    respond_to do |format|
+      format.html { render :contact, locals: { feedback: {} } }
+    end
+  end
+
+  def leave_feedback
+    required = [:name, :email, :reply, :feedback_type, :message]
+    form_complete = true
+
+    required.each do |f|
+      if params.has_key? f and not params[f].blank?
+        # do nothing
+      else
+        form_complete = false
+      end
+    end
+
+    if form_complete
+      form_status_msg = 'Thank you for your feedback!'
+    else
+      form_status_msg = 'Please fill in all the remaining form fields and resubmit'
+    end
+
+
+    respond_to do |format|
+      format.html { render :contact, locals: { status_msg: form_status_msg, feedback: params } }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root to: redirect('/welcome', status: 302)
+
   get 'welcome', to: 'static_pages#welcome', as: 'welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'static_pages#about', as: 'about'
+
+  get 'contact', to: 'static_pages#contact', as: 'contact'
+
+  post 'contact', to: 'static_pages#leave_feedback', as: 'leave_feedback'
 end
