b8aa357c85d076cf991750c4fb901e87c73a3944
wscarter <wescarterdev@fastmail.com>
2020-02-11 20:20:15 -0600

    welcome test fix

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get welcome" do
-    get static_pages_welcome_url
-    assert_response :success
+    get root_path
+    assert_response :redirect
   end
 
 end
